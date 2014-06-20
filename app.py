0f8bebdc03f3063ab792ed6c78a2f628c88993f5
kmlg <liu8889909295@gmail.com>
2014-06-20 19:47:49 +0800

    mongodb models

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
- 
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
 from micawber import bootstrap_basic
-from peewee import SqliteDatabase
- 
-APP_ROOT = os.path.dirname(os.path.realpath(__file__))
-DATABASE = os.path.join(APP_ROOT, 'notes.db')
-DEBUG = False
- 
+
+# Create app
 app = Flask(__name__)
-app.config.from_object(__name__)
-db = SqliteDatabase(app.config['DATABASE'], threadlocals=True)
+
+# MongoDB Config
+app.config['MONGODB_DB'] = 'notes'
+app.config['MONGODB_HOST'] = 'localhost'
+app.config['MONGODB_PORT'] = 27017
+
+# Create database connection object
+db = MongoEngine(app)
+
 oembed = bootstrap_basic()
+
+if __name__ == '__main__':
+    app.run()
--- models.py
+++ models.py
@@ -1,34 +1,29 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import datetime
- 
+from app import db, oembed
 from flask import Markup
 from markdown import markdown
 from micawber import parse_html
-from peewee import *
- 
-from app import db, oembed
- 
-class Note(Model):
-    content = TextField()
-    timestamp = DateTimeField(default=datetime.datetime.now)
-    archived = BooleanField(default=False)
- 
-    class Meta:
-        database = db
- 
+
+class Note(db.Document):
+    content = db.StringField(required=True)
+    timestamp = db.DateTimeField(default=datetime.datetime.now,
+            required=True)
+    archived = db.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.content
+
     def html(self):
         html = parse_html(
-            markdown(self.content),
-            oembed,
-            maxwidth=300,
-            urlize_all=True)
+                markdown(self.content),
+                oembed,
+                maxwidth=300,
+                urlize_all=True
+                )
         return Markup(html)
- 
-    @classmethod
-    def public(cls):
-        return (Note
-                .select()
-                .where(Note.archived == False)
-                .order_by(Note.timestamp.desc()))
+
+    meta = {
+            'ordering':['-timestamp']
+            }
