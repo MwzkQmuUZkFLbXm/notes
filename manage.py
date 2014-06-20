29fd2d586913aaff4b5341d617a239adf2a4129d
kmlg <liu8889909295@gmail.com>
2014-06-20 22:04:38 +0800

    merge mongo

--- manage.py
+++ manage.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from app import app
 from models import Note
 import views
- 
+
 if __name__ == '__main__':
-    Note.create_table(True)
-    app.run(debug=True)
+    app.run()
